9323824e8f30cf6f26fa3f37adf4cf02c2823dde
Stephane Thomas <thomas.stephn@gmail.com>
2013-07-09 09:31:28 -0700

    Add js toggle for portfolio link new file:   app/assets/javascripts/main.js modified:   app/assets/stylesheets/main_components.scss modified:   app/views/homepage/show.html.erb

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('.welcome').on('mouseenter', ['.middle', '.last'], function() {
+    $('.last .bracket').animate({
+      height: '58px'
+    }, 800);
+    $('.last .bracket p').slideDown("slow");
+  });
+  $('.welcome').on('mouseleave', ['.middle', '.last'], function() {
+    $('.last .bracket').animate({
+      height: '18px'
+    }, 800);
+    $('.last .bracket p').slideUp("slow");
+  });
+});
