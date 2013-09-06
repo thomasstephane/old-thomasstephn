e21df94cfb23ae13901f93258a610268817b0edd
Stephane Thomas <thomas.stephn@gmail.com>
2013-09-06 21:16:11 +0200

    Correct slide bug

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,30 +1,30 @@
 $(document).ready(function() {
   $('.welcome').hover(function() {
-    $('.last .bracket').animate({
+    $('.last .bracket').stop().animate({
       height: '58px'
     }, 800);
-    $('.last .bracket p').slideDown("slow");
-    $('.slide').animate({
+    $('.last .bracket p').stop().slideDown("slow");
+    $('.slide').stop().animate({
       top: '136px'
     }, 800);
   });
   $('.welcome').on('mouseleave', function() {
-    $('.last .bracket').animate({
+    $('.last .bracket').stop().animate({
       height: '18px'
     }, 800);
-    $('.slide').animate({
+    $('.slide').stop().animate({
       top: '96px'
     }, 800);
-    $('.last .bracket p').slideUp("slow");
+    $('.last .bracket p').stop().slideUp("slow");
   });
 
   $('.menuImage').hover(function(){
-    $(this).parent().animate({
+    $(this).parent().stop().animate({
       'padding-bottom' : '0px'
     }, 500);
   });
   $('.menuImage').on('mouseleave',function(){
-    $(this).parent().animate({
+    $(this).parent().stop().animate({
       'padding-bottom' : '69px'
     }, 500);
   });
